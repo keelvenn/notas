f986ca5f24096d46d53f39fb567cf8a27eb7e8d7
Kelven <kelvenboss123@gmail.com>
2021-10-19 08:37:08 -0300

    Atualização dos Botões.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const add = document.getElementById("adicionar");
 const body = document.getElementById("body");
+const note = document.querySelector(".text").cloneNode(true);
 
 
 add.addEventListener("click", function(){
-
+body.append(note);
 });
