09441e9074845710f245139bcc665a3701079174
Kelven <kelvenboss123@gmail.com>
2021-11-12 20:48:31 -0300

    Refatoração toal
    - Finalmente foi implementado a atualização dos textos e carregamento dos mesmos de forma perfeita
    - Refatoração total para melhor entendimento.

--- script.js
+++ script.js
@@ -1,137 +1,106 @@
 const adicionar = document.getElementById("adicionar");
 const notas = document.getElementById("notas");
+let lists = [];
+let index = 0;
+
+if(!localStorage.data){
+  localStorage.data = "[]";
+}
+
+if(!localStorage.index){
+  localStorage.index = "[]";
+}
 
 
-
-criarnota();
 loadnote();
 
+adicionar.addEventListener("click",function(){
+adicionar_nota();
+
+});
+
+
+function adicionar_nota(event){
+
+  const container = document.createElement("div");
+  container.setAttribute("id","container");
+
+  const containerchk = document.createElement("div");
+  containerchk.setAttribute("id","containerchk");
+
+  const headernota = document.createElement("div");
+  headernota.setAttribute("id","headernota");
+
+  const textarea = document.createElement("textarea");
+
+  const apagar = document.createElement("button");
+  apagar.setAttribute("id","apagar");
+
+  const expandir = document.createElement("input");
+  expandir.type = "checkbox";
+  expandir.setAttribute("id","expandir");
+
+  const exp = document.createElement("button");
+  exp.setAttribute("id","exp");
+  exp.innerText = "-";
+
+  const check = document.createElement("button");
+  check.setAttribute("id","check");
+  check.innerText = "✔";
+
+  containerchk.append(expandir,check,exp);
+
+  apagar.innerText = "X";
+
+  headernota.append(containerchk,apagar);
+
+  container.append(headernota, textarea);
+
+  notas.append(container);
+
+  //Cria uma lista com valor do Index
+  //if(!lists[index]){
+  //  lists[index] = index;
+  //}
+   }; // Final função
 
 
 
-//Funções
-function criarnota(){
 
-    adicionar.addEventListener("click",function(){
-    const container = document.createElement("div");
-    container.setAttribute("id","container");
+function target(event){
+  let target = event.target;
 
-    const containerchk = document.createElement("div");
-    containerchk.setAttribute("id","containerchk");
-
-    const headernota = document.createElement("div");
-    headernota.setAttribute("id","headernota");
-
-    const textarea = document.createElement("textarea");
-
-    const apagar = document.createElement("button");
-    apagar.setAttribute("id","apagar");
-
-    const expandir = document.createElement("input");
-    expandir.type = "checkbox";
-    expandir.setAttribute("id","expandir");
-
-    const exp = document.createElement("button");
-    exp.setAttribute("id","exp");
-    exp.innerText = "-";
-
-    const check = document.createElement("button");
-    check.setAttribute("id","check");
-    check.innerText = "✔";
-
-    containerchk.append(expandir,check,exp);
-
-    apagar.innerText = "X";
-
-    headernota.append(containerchk,apagar);
-
-    container.append(headernota, textarea);
-
-    notas.append(container);
-
-
-      //TextArea
-      textarea.addEventListener("blur", function(){
-        let texts = textarea.value;
-
-        textarea.style.fontFamily = "Arial";
-        textarea.style.fontSize = "20px";
-        check.style.display = "none";
-
-        //Lógica para Salvar
-        if(localStorage.data != null){
-            let list_storage = JSON.parse(localStorage.data);
-            list_storage.push(textarea.value);
-            localStorage.data = JSON.stringify(list_storage);
-        }else{
-          localStorage.data = "[]";
-        }
-
-        console.log(localStorage.data);
-
-      });
-
-      textarea.addEventListener("focus", function(){
-        textarea.style.fontSize = "15px";
-        check.style.display = "block";
-      });
-
-
-    //Funções dos Botões
-      apagar.addEventListener("click",function(){
-        let list_storage = JSON.parse(localStorage.data);
-
-        //list_storage.splice(list_storage.indexOf(textarea.value),1);
-        container.remove();
-      });
-
-    /////////////////////////////////////////
-      exp.addEventListener("click",function(){
-      if(expandir.checked == true){
-         expandir.checked = false;
-      }else if(expandir.checked == false){
-        expandir.checked = true;
-      }
-
-      if(expandir.checked == true ){
-        textarea.style.width = "500px";
-        textarea.style.height = "500px";
-        headernota.style.width = "500px";
-      }else{
-        textarea.style.width = "200px";
-        textarea.style.height = "200px";
-        headernota.style.width = "200px";
-      }
-
-
-      });
-
-      //////////////////////////////////////////////
-      check.addEventListener("click",function(){
-        textarea.onblur();
-      })
-
-      });
-
-
-};// Final função criar nota;
-
-function savenote(){
-
-  if(localStorage.data != null){
-      let list_storage = JSON.parse(localStorage.data);
-      list_storage.push(textarea.value);
-      localStorage.data = JSON.stringify(list_storage);
-  }else{
-    localStorage.data = "[]";
+  //TextArea recebe os id's
+  if(!target.id ){
+    target.id = index;
+    index = index + 1;
   }
-};
+
+  //console.log("TextArea ID: "target.id);
+
+    target.addEventListener("blur",function(event){
+    lists[target.id] = target.value;
+
+    push_notes();
+    //Coloca o valor do textvalue na lista
+
+    }) };
+
+
+function push_notes(event){
+
+  let list_storage = JSON.parse(localStorage.data);
+
+  list_storage = lists;
+
+  localStorage.data = JSON.stringify(list_storage);
+
+ };
 
 function loadnote(){
   let list_storage = JSON.parse(localStorage.data);
 
-  list_storage.forEach(function(i){
-
+  list_storage.forEach(function(list,index){
     const container = document.createElement("div");
     container.setAttribute("id","container");
 
@@ -142,7 +111,9 @@ function loadnote(){
     headernota.setAttribute("id","headernota");
 
     const textarea = document.createElement("textarea");
-    textarea.value = i;
+    textarea.value = list_storage[index];
+    lists.push(list);
+
     const apagar = document.createElement("button");
     apagar.setAttribute("id","apagar");
 
@@ -168,7 +139,25 @@ function loadnote(){
 
     notas.append(container);
 
-  })
-};
+    console.log(lists);
 
-console.log(localStorage.data);
+    textarea.id = index;
+
+    //Cria uma lista com valor do Index
+    if(!lists[index]){
+      lists[index] = index;
+      } // Final função
+    });
+    push_notes();
+    };
+
+function apagar_nota(){
+  let list_storage = JSON.parse(localStorage.data);
+
+  let index_list = list_storage.indexOf(textarea.value, 1);
+  list_storage.splice(index_list);
+
+  localStorage.data = JSON.stringify(list_storage);
+
+  container.remove();
+};
